3fdf6096d80daffbeeaf21566100bee18fcffc4a
Ferhatcakmakogluu <ferhat.cakmakoglu61@gmail.com>
2024-08-03 23:39:16 +0300

    AppDbContext creted

---
+++ NLayer.Repository/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NLayer.Core.Entities;
+
+namespace NLayer.Repository
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {  
+        }
+
+        DbSet<Admins> Admins { get; set; }
+        DbSet<Albume> Albumes { get; set; }
+        DbSet<News> News { get; set; }
+        DbSet<NewsComments> NewsComments { get; set; }
+        DbSet<PictureComments> PictureComments { get; set; }
+        DbSet<Pictures> Pictures { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
